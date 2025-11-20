4d8511d9b97f61b1d0443f94678f32e1e4f6ff20
Curroll <dhankarprince02@gmail.com>
2025-11-20 23:55:09 +0530

    login and logout implemented

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -61,4 +61,44 @@ export const signUp = async (req, res) => {
       console.error("Error during sign up:", error);
       res.status(500).json({message: "Internal Server Error"});
     }
+}
+
+export const login = async (req, res) =>{
+  const {email,password} = req.body;
+try {
+  if(!email || !password){
+    return  res.status(400).json({message: "All fields are required"});
   }
+  const user = await User.findOne({email})
+  if(!user){
+    return res.status(400).json({message: "Invalid credentials"});
+  }
+
+  const isCorrectPassword = bcrypt.compareSync(String(password), user.password);
+  if(!isCorrectPassword){
+    return res.status(400).json({message: "Invalid credentials"});
+  }
+  generateToken(user._id , res);
+
+  res.status(200).json({
+    _id: user._id,
+    fullName: user.fullName,
+    email: user.email,
+    profilePicture: user.profilePicture
+  });
+
+} catch (error) {
+  console.error("Error during login:", error);
+  res.status(500).json({message: "Internal Server Error"});
+}
+  
+};
+
+export const logout = async (req, res) =>{
+  res.cookie('jwt','',{
+    maxAge:0,
+  });
+  res.status(200).json({message: "Logged out successfully"});
+};
+
+  

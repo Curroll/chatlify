3d72564ccfbdc483db3ec64e969a391ff825e86a
Curroll <dhankarprince02@gmail.com>
2025-11-21 00:03:26 +0530

    some changes in .env

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -9,6 +9,7 @@ CLIENT_URL: process.env.CLIENT_URL,
 RESEND_API_KEY: process.env.RESEND_API_KEY,
 EMAIL_FROM: process.env.EMAIL_FROM,
 EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+CLIENT_URL: process.env.CLIENT_URL
 };
 // PORT=5001
 // NODE_ENV=development

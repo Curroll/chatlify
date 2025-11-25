8d6f3aef198a0aec61951271bd5bcedcc3d9e836
Curroll <dhankarprince02@gmail.com>
2025-11-25 13:41:51 +0530

    arcjet rate limiter implemented

---
+++ backend/src/lib/arcjet.js
@@ -0,0 +1,30 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+import { ENV } from "./env.js";
+
+const aj = arcjet({
+  key: ENV.ARCJET_API_KEY,
+  rules: [
+    // Protect against injections
+    shield({ mode: "LIVE" }),
+
+    // Bot + Spoofed Bot detection
+    detectBot({
+      mode: "LIVE",
+      blockSpoofed: true,
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Googlebot, Bingbot, etc.
+        // "CATEGORY:MONITOR",
+        // "CATEGORY:PREVIEW",
+      ],
+    }),
+
+    // Rate Limiting
+    slidingWindow({
+      mode: "LIVE",
+      max: 100,
+      interval: 60,
+    }),
+  ],
+});
+
+export default aj;
